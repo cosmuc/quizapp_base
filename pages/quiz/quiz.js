a9d64e51a95ddac24bfc8b2dc417194c98ff44f0
Pedro Ramos <pdr.ramos.duarte@gmail.com>
2024-06-06 18:37:40 +0000

    funcao proxima pergunta e de finalizar

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,5 +1,5 @@
-import { trocarTema, verificarTema } from "../../helpers/tema-helper.js"
 
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
@@ -7,106 +7,106 @@ const assunto = localStorage.getItem("assunto")
 
 let quiz = {}
 let pontos = 0
-let perguntas =1
+let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
-let respostaCorretaId = ""
+let respostaCorretaId
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
-verificarTema(body,botaoTema)
+verificarTema(body, botaoTema)
 
-
-
-function alterarAssunto(){
+function alterarAssunto() {
     const divIcone = document.querySelector(".assunto_icone")
     const iconeImg = document.querySelector(".assunto_icone img")
     const assuntoTitulo = document.querySelector(".assunto h1")
 
-    divIcone.classList.add(assunto.toLocaleLowerCase())
+    divIcone.classList.add(assunto.toLowerCase())
     iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
     iconeImg.setAttribute("alt", `icone de ${assunto}`)
     assuntoTitulo.innerText = assunto
 }
 
-
- async function buscarPerguntas(){
+async function buscarPerguntas() {
     const urlDados = "../../data.json"
 
-    await fetch(urlDados).then(resposta => respostas.json()).then(dados => {
-        dados.quizzes.array.forEach(dado=> {
+    await fetch(urlDados).then(resposta => resposta.json()).then(dados => {
+        dados.quizzes.forEach(dado => {
             if (dado.title === assunto) {
                 quiz = dado
             }
-        });
+        })
     })
+    console.log(quiz)
 }
 
-function montarPergunta(){
+function montarPergunta() {
     const main = document.querySelector("main")
 
-    main.innerHTML = ` 
-            <section class="pergunta">
-                <p>Questão ${perguntas} de 10</p>
+    main.innerHTML = `
+        <section class="pergunta">
+                <div>
+                    <p>Questão ${pergunta} de 10</p>
 
-                <h2>${alterarSinais(quiz.questions[perguntas-1].questions)}</h2>
+                    <h2>${alterarSinais(quiz.questions[pergunta-1].question)}</h2>
+                </div>
 
                 <div class="barra_progresso">
-                        <div style="width: ${perguntas * 10}%"></div>
+                    <div style="width: ${pergunta * 10}%"></div>
                 </div>
             </section>
 
             <section class="alternativas">
                 <form action="">
                     <label for="alternativa_a">
-                        <input type="radio" id="alternativa_a" name="alternativa" value="${alterarSinais(quiz.questions[perguntas-1].options[0])}">
+                        <input type="radio" id="alternativa_a" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[0])}">
 
                         <div>
                             <span>A</span>
-                            ${alterarSinais(quiz.questions[perguntas-1].options[0])}
+                            ${alterarSinais(quiz.questions[pergunta-1].options[0])}
                         </div>
                     </label>
-                    
+
                     <label for="alternativa_b">
-                        <input type="radio" id="alternativa_b" name="alternativa" value="${alterarSinais(quiz.questions[perguntas-1].options[1])}">
+                        <input type="radio" id="alternativa_b" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[1])}">
 
                         <div>
                             <span>B</span>
-                            ${alterarSinais(quiz.questions[perguntas-1].options[1])}
+                            ${alterarSinais(quiz.questions[pergunta-1].options[1])}
                         </div>
                     </label>
 
                     <label for="alternativa_c">
-                        <input type="radio" id="alternativa_c" name="alternativa" value=" ${alterarSinais(quiz.questions[perguntas-1].options[2])}">
+                        <input type="radio" id="alternativa_c" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[2])}">
 
                         <div>
                             <span>C</span>
-                            ${alterarSinais(quiz.questions[perguntas-1].options[2])}
+                            ${alterarSinais(quiz.questions[pergunta-1].options[2])}
                         </div>
                     </label>
 
                     <label for="alternativa_d">
-                        <input type="radio" id="alternativa_d" name="alternativa" value=" ${alterarSinais(quiz.questions[perguntas-1].options[3])}">
+                        <input type="radio" id="alternativa_d" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[3])}">
 
                         <div>
                             <span>D</span>
-                            ${alterarSinais(quiz.questions[perguntas-1].options[3])}
+                            ${alterarSinais(quiz.questions[pergunta-1].options[3])}
                         </div>
                     </label>
                 </form>
 
-                <button>Enviar</button>
+                <button>Responder</button>
             </section>
-`
+    `
 }
 
-function alterarSinais(texto){
-    return texto.relace(/</g, "&lt;").replace(/>/g, "&gt;")
+function alterarSinais(texto) {
+    return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 }
 
-function guardarResosta(evento){
+function guardarResposta(evento) {
     resposta = evento.target.value
     idInputResposta = evento.target.id
 
@@ -114,28 +114,59 @@ function guardarResosta(evento){
     botaoEnviar.addEventListener("click", validarResposta)
 }
 
-function validarResposta(){
-    if (resposta === quiz.questions[pergunta-1].answer){
+function validarResposta() {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+
+    if (pergunta === 10) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
+    if (resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
-        pontos = pontos+1
-    } else{
+        pontos = pontos + 1
+    } else {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
+    pergunta = pergunta + 1
 }
 
-async function iniciar(){
-    alterarAssunto()
-    await buscarPerguntas()
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
+
+    window.location.href = "../resultado/resultado.html"
+}
+
+function proximaPergunta() {
     montarPergunta()
+    adicionarEventosInputs()
+}
 
-    const inputsRespostas = document.querySelectorAll(".alternativas input")
-    inputsRespostas.forEach(input =>{
-        input.addEventListener("click", guardarResosta)
+function adicionarEventosInputs() {
+    const inputsResposta = document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click", guardarResposta)
 
-        if (input.value === quiz.questions[pergunta-1].answer)
+        if (input.value === quiz.questions[pergunta-1].answer) {
             respostaCorretaId = input.id
+        }
     })
 }
 
+async function iniciar() {
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventosInputs()
+}
+
 iniciar()
+
+
+//tive que reescrever esse codigo de novo pq tinha um problema e nao sabia onde que ele estava no codigo eu vou enlouquecer
